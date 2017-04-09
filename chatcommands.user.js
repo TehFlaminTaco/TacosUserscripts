e8f1ae69e73a52b3fa8e5badc8a5a72d88a61d6b
William Lemon <William.Lemon2@gmail.com>
2017-04-10 09:38:21 +1000

    Bnup

--- chatcommands.user.js
+++ chatcommands.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chat Commands
 // @namespace    http://tampermonkey.net/
-// @version      0.2.2
+// @version      0.2.3
 // @description  Add some simple ascii art commands.
 // @author       Teh Flamin' Taco
 // @match        *://*chat.stackexchange.com/*
