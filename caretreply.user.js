c914a1e38e2d93fe41f0cd247a1bec11f27cfdfc
William Lemon <William.Lemon2@gmail.com>
2017-04-21 14:40:47 +1000

    Updated Caret Reply, Chat Preview and Plugin Interface
    Now you can reply to a message based on it's contents, like `^?Hey
    @ATaco? Hey friend!`

--- caretreply.user.js
+++ caretreply.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Caret Reply
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Automatically reply to messages by using ^Message
 // @author       You
 // @match        *://*chat.stackexchange.com/*
@@ -13,7 +13,67 @@
 (function() {
     'use strict';
     // Your code here...
-    window.taco_storedMessages = false;
+    window.taco = window.taco || {installedScripts: []};
+    window.taco.installedScripts[window.taco.installedScripts.length] = "caretReply"
+    window.taco.caretReply = {};
+    var caretReply = window.taco.caretReply;
+    caretReply.storedMessages = false;
+    caretReply.caretRegex = /^(\^\*(\d+)|\^\?(.*?\?+)|(\^+))(.*)/;
+
+    caretReply.indexers = []; // getMessage, which returns a text, uses indexers to find the reply. If match 2 is valid, indexer 0 is used. If 3 is valid, than 1. And so on.
+
+    caretReply.indexers[0] = function(txt){
+        return caretReply.storedMessages.length - Number(txt);
+    }
+
+    caretReply.indexers[1] = function(txt){
+        // I stole this from Stack Overflow.
+        // Thanks Bobince.
+        // http://stackoverflow.com/a/3561711/7170955
+        txt = txt.match(/(.*?)(\?+)/);
+        var newRegex = txt[1].toLowerCase()
+                          .replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+                          .split(/\\\*/)
+                          .join(".*");
+
+        var c_count = txt[2].length;
+        var i = 0;
+
+        for(var i=caretReply.storedMessages.length-1; i >= 0; i--){
+            if(caretReply.storedMessages[i].textContent.toLowerCase().match(newRegex) || $(caretReply.storedMessages[i]).parent().parent().find(".tiny-signature").find(".username").text().toLowerCase().match(newRegex)){
+                c_count--;
+                if(c_count<=0){
+                    return i;
+                }
+            }
+        }
+        return 0;
+    }
+
+    caretReply.indexers[2] = function(txt){
+        return caretReply.storedMessages.length - txt.length;
+    }
+
+    caretReply.getMessageText = function(inp){
+        var matches = inp.match(caretReply.caretRegex);
+        if(matches){
+            return matches[matches.length-1];
+        }
+        return inp;
+    }
+
+    caretReply.getMessage = function(inp){
+        var matches = inp.match(caretReply.caretRegex);
+        if(matches){
+            for(var c=2; c<matches.length-1; c++){
+                if(matches[c]){
+                    return caretReply.storedMessages[caretReply.indexers[c-2](matches[c])];
+                }
+            }
+        }
+        return false;
+    }
+
     $.fn.extend({
         bindAs: function (nth, type, data, fn) {
             if (type.indexOf(' ') > -1) {
@@ -64,29 +124,35 @@
         }
     });
 
-	var input = $("#input");
+    var input = $("#input");
 
-	input.keyup(function(event){
-		if(input.val().match(/^$/)){
-			taco_storedMessages = false;
-		}else if(!taco_storedMessages){
-			taco_storedMessages = $("[id^=message]");
-		}
-		if(input.val().match(/^\^/)){
-			var carets = input.val().match(/^\^+/)[0];
-		}
-	});
+    input.keyup(function(event){
+        if(input.val().match(/^$/)){
+            caretReply.storedMessages = false;
+        }else if(!caretReply.storedMessages){
+            caretReply.storedMessages = $("[id^=message]");
+        }
+    });
 
-	input.bindAs(0,'keydown',function(event){
-		if(event.which==13){
-			var text = input.val().replace(/^\^\*(\d+)/, function(a,b){return ("^").repeat(Number(b));});
-			var carets = text.match(/^\^+/);
-			if(!carets){
-				return;
-			}
-			var carets = carets[0];
+    input.bindAs(0,'keydown',function(event){
+        if(event.which==13){
+            var text = input.val();
+            var msg = caretReply.getMessage(text);
+            if(!msg){
+                return;
+            }
 
-			input.val(text.replace(/^\^+/,":"+taco_storedMessages[taco_storedMessages.length - carets.length].getAttribute("id").replace(/message-/,"")+" "));
-		}
-	});
+            input.val(msg.getAttribute("id").replace(/message-/, ":") + caretReply.getMessageText(text));
+            /*
+            var text = input.val().replace(/^\^\*(\d+)/, function(a,b){return ("^").repeat(Number(b));});
+            var carets = text.match(/^\^+/);
+            if(!carets){
+                return;
+            }
+            var carets = carets[0];
+
+            input.val(text.replace(/^\^+/,":"+taco_storedMessages[taco_storedMessages.length - carets.length].getAttribute("id").replace(/message-/,"")+" "));
+            */
+        }
+    });
 })();
--- sechatpreview.user.js
+++ sechatpreview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chat Preview
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  Preivew SE chat before posting!
 // @author       You
 // @match        *://chat.stackexchange.com/rooms/*
@@ -9,28 +9,35 @@
 // @grant        none
 // ==/UserScript==
 
-function markdownTaco(s){
-    var wrap_left = "";
-    var wrap_right = "";
-    s = s.replace(/^\^\*(\d+)/, function(a,b){return ("^").repeat(Number(b))});
-    if(s.match(/^\^/) && (typeof taco_storedMessages!='undefined')){
-        var carets = s.match(/^\^+/)[0];
-        var message = $(taco_storedMessages[taco_storedMessages.length - carets.length]);
-        var user_name = message.parent().parent().find(".tiny-signature").find(".username").text();
-        wrap_left = "<b>" + user_name + "</b><br><b style='color:gray'>"+message.text() + "</b><br><br>";
-        s = s.replace(/^\^+/, "");
-    }
-    if(s.match(/(gif|png|jpg|jpeg|bmp|svg)$/i)){
-        return "<img src="+s+" />";
-    }
-    return wrap_left + markdownMini(s) + wrap_right;
-}
-
 
 (function() {
     'use strict';
 
     // Your code here...
+
+    window.taco = window.taco || {installedScripts: []};
+    window.taco.installedScripts[window.taco.installedScripts.length] = "chatPreview"
+    window.taco.chatPreview = {};
+    var chatPreview = window.taco.chatPreview;
+
+    chatPreview.markdownTaco = function(s){
+        var wrap_left = "";
+        var wrap_right = "";
+
+        if(window.taco.installedScripts.find(x=>x=="caretReply")){
+            var replyTo = window.taco.caretReply.getMessage(s);
+            if(replyTo){
+                replyTo = $(replyTo);
+                var usr_name = replyTo.parent().parent().find(".tiny-signature").find(".username").text();
+                wrap_left = `<b>${usr_name}</b><br><b style='color:gray;'>${replyTo.text()}</b><br><br>`;
+                s = taco.caretReply.getMessageText(s);
+            }
+        }
+        if(s.match(/(gif|png|jpg|jpeg|bmp|svg)$/i)){
+            return "<img src="+s+" />";
+        }
+        return wrap_left + markdownMini(s) + wrap_right;
+    }
 
     $("#main").append(`<div id="chat-preview" style="position:fixed;bottom:80px;margin-bottom:12px;border:solid;border-radius:4px;background:white;padding:4px;display:none"></div>`);
     var stored="";
@@ -51,7 +58,7 @@ function markdownTaco(s){
             stored = S;
             timer=setTimeout(function(){
                 timer=-1;
-                chat_prev.innerHTML = markdownTaco(S);
+                chat_prev.innerHTML = chatPreview.markdownTaco(S);
             },500);
         }
     },30);
