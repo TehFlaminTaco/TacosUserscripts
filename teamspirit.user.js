109c2ecefddcaf72cd77e74bdd9f20ac373b36c9
Ryan R <Mego@users.noreply.github.com>
2017-04-26 06:35:06 -0500

    Colored squares and cleanup
    - Colored squares next to usernames instead of coloring the usernames so that mods can be distinguished (not the cleanest solution but it will do)
    - Semicolons never hurt anybody
    - Empty try-catch blocks have hurt a lot of people

--- teamspirit.user.js
+++ teamspirit.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Team Spirit!
 // @namespace    http://tampermonkey.net/
-// @version      1.3
-// @description  Actually hate eachother for no reason.
+// @version      1.4
+// @description  Actually hate each other for no reason.
 // @author       Teh Flamin' Taco
+// @contributer  Mego
 // @match        *://*chat.stackexchange.com/*
 // @match        *://*chat.stackoverflow.com/*
 // @grant        none
@@ -16,40 +17,39 @@
 	setInterval(function(){
 	var x=$(".user-container");
 	x.each((i,a)=>{
-		try{
-			var names = $(a).find(".username");
-			if(names.length > 0){
-				names.css({color : a.getAttribute("class").match(/\d+/)[0]%2==0 ? "#F00" : "#00F"})
-			}
-		}catch(e){}
-	})
+        var names = $(a).find(".username:visible");
+        var icons = $(a).find(".teamspirit:visible");
+        if(icons.length > 0) { return; }
+        if(names.length > 0){
+            names.before("<div class='teamspirit' id='teamspirit-icon' style='color: " + (a.getAttribute("class").match(/\d+/)[0]%2===0 ? "#F00" : "#00F") + "'>■</div>");
+            //names.css({color : a.getAttribute("class").match(/\d+/)[0]%2===0 ? "#F00" : "#00F"});
+        }
+	});
 	$(".votesummary .collapsible").children()
 	.each(
 		(a,b)=>{
 			var links = $(b).find('a');
-			$(links[links.length-1]).css({["color"] : links[links.length-1].getAttribute("href").match(/users\/(\d+)/)[1]%2==0 ? "#F00" : "#00F"});
+			$(links[links.length-1]).css({["color"] : links[links.length-1].getAttribute("href").match(/users\/(\d+)/)[1]%2===0 ? "#F00" : "#00F"});
 		}
-	)},100);
+	);},100);
 
 	setInterval(function(){
 		var x = $(".user-container");
 		var cRed = 0;
 		var cBlue = 0;
 		for(var i=0; i < x.length; i++){
-			try{
-		    	if(x[i].getAttribute("class").match(/\d+/)[0]%2==0){cRed++}else{cBlue++}
-			}catch(e){}
+            if(x[i].getAttribute("class").match(/\d+/)[0]%2===0){cRed++;}else{cBlue++;}
 		}
 
 		$(".votesummary .collapsible").children()
 		.each(
 			(a,b)=>{
-				var votes = $(b).find(".vote-count-container .times").text()
+				var votes = $(b).find(".vote-count-container .times").text();
 				var links = $(b).find('a');
-				if(votes==""){votes = 1}
-				if (links[links.length-1].getAttribute("href").match(/users\/(\d+)/)[1]%2==0){cRed+=votes*10}else{cBlue+=votes*10}
+				if(votes===""){votes = 1;}
+				if (links[links.length-1].getAttribute("href").match(/users\/(\d+)/)[1]%2===0){cRed+=votes*10;}else{cBlue+=votes*10;}
 			}
-		)
+		);
 		var total = cRed + cBlue;
 
 		var redPercent = Math.ceil((cRed / total)*100);
@@ -58,7 +58,7 @@
 		$("#bluescore").css({width:`${bluPercent}%`});
 	},2000);
 
-	$(".chat-input").append(`<div id="scoreholder"><div id="redscore" class="scoreholder"></div><div id="bluescore" class="scoreholder"></div></div>`)
+	$(".chat-input").append(`<div id="scoreholder"><div id="redscore" class="scoreholder"></div><div id="bluescore" class="scoreholder"></div></div>`);
 
 	$("head").append(`<style>
 #scoreholder{
@@ -82,6 +82,6 @@
 	background-color:#00F;
 	width:50%;
 }
-</style>`)
+</style>`);
 
 })();
