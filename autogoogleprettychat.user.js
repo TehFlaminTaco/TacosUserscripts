58b2c948099948285fdddb75ee45d0937c63e966
somebody <ehern.lee@gmail.com>
2017-04-27 00:30:26 +0000

    Much better

--- autogoogleprettychat.user.js
+++ autogoogleprettychat.user.js
@@ -1,102 +1,78 @@
 // ==UserScript==
 // @name         Prettify Chat
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.2.1
 // @description  Use Google Prettifier on Stack Exchange's chat's codeblocks.
 // @author       Teh Flamin' Taco
-// @match        *://*chat.stackexchange.com/*
-// @match        *://*chat.stackoverflow.com/*
+// @match        *://chat.stackexchange.com/*
+// @match        *://chat.stackoverflow.com/*
 // @grant        none
 // ==/UserScript==
 
+/* global $, PR */
 (function() {
     'use strict';
-
     // Load Google Prettify
-    var prettyScript = document.createElement("script");
-    prettyScript.setAttribute("src", "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js");
-    document.head.appendChild(prettyScript);
+    var script = document.createElement("script");
+    script.setAttribute("src", "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js");
+    document.head.appendChild(script);
 
-    var extensions = ["/google/code-prettify/blob/master/src/lang-apollo.js",
-                    "/google/code-prettify/blob/master/src/lang-basic.js",
-                    "/google/code-prettify/blob/master/src/lang-clj.js",
-                    "/google/code-prettify/blob/master/src/lang-css.js",
-                    "/google/code-prettify/blob/master/src/lang-dart.js",
-                    "/google/code-prettify/blob/master/src/lang-erlang.js",
-                    "/google/code-prettify/blob/master/src/lang-go.js",
-                    "/google/code-prettify/blob/master/src/lang-hs.js",
-                    "/google/code-prettify/blob/master/src/lang-lasso.js",
-                    "/google/code-prettify/blob/master/src/lang-lisp.js",
-                    "/google/code-prettify/blob/master/src/lang-llvm.js",
-                    "/google/code-prettify/blob/master/src/lang-logtalk.js",
-                    "/google/code-prettify/blob/master/src/lang-lua.js",
-                    "/google/code-prettify/blob/master/src/lang-matlab.js",
-                    "/google/code-prettify/blob/master/src/lang-ml.js",
-                    "/google/code-prettify/blob/master/src/lang-mumps.js",
-                    "/google/code-prettify/blob/master/src/lang-n.js",
-                    "/google/code-prettify/blob/master/src/lang-pascal.js",
-                    "/google/code-prettify/blob/master/src/lang-proto.js",
-                    "/google/code-prettify/blob/master/src/lang-r.js",
-                    "/google/code-prettify/blob/master/src/lang-rd.js",
-                    "/google/code-prettify/blob/master/src/lang-rust.js",
-                    "/google/code-prettify/blob/master/src/lang-scala.js",
-                    "/google/code-prettify/blob/master/src/lang-sql.js",
-                    "/google/code-prettify/blob/master/src/lang-swift.js",
-                    "/google/code-prettify/blob/master/src/lang-tcl.js",
-                    "/google/code-prettify/blob/master/src/lang-tex.js",
-                    "/google/code-prettify/blob/master/src/lang-vb.js",
-                    "/google/code-prettify/blob/master/src/lang-vhdl.js",
-                    "/google/code-prettify/blob/master/src/lang-wiki.js",
-                    "/google/code-prettify/blob/master/src/lang-xq.js",
-                    "/google/code-prettify/blob/master/src/lang-yaml.js"];
-    
-    for (var ext = 0; ext < extensions.length; ext++){
-        var prettyScript = document.createElement("script");
-        prettyScript.setAttribute("src", extensions[ext]);
-        document.head.appendChild(prettyScript);
+    var langs = [
+        "apollo", "basic", "clj", "css", "dart", "erlang", "go", "hs", "lasso",
+        "lisp", "llvm", "logtalk", "lua", "matlab", "ml", "mumps", "n",
+        "pascal", "proto", "r", "rd", "rust", "scala", "sql", "swift", "tcl",
+        "tex", "vb", "vhdl", "wiki", "xq", "yaml"
+    ];
+
+    for (var i = 0; i < langs.length; i++) {
+        script = document.createElement("script");
+        script.setAttribute(
+            "src",
+            "/google/code-prettify/blob/master/src/lang-" + langs[i] + ".js"
+        );
+        document.head.appendChild(script);
     }
 
     // Update all them blocks.
-    function updatePretty(){
-    	var codeBlocks = $("pre, code");
-    	var rePretty = false;
-    	for (var block = 0; block < codeBlocks.length; block++){
-    		var codeBlock = codeBlocks[block];
-    		var curClass = codeBlock.getAttribute('class')||"";
-    		if (!curClass.includes("prettyprint")){
-    			rePretty = true;
-    			var found = $(codeBlock).text().toLowerCase().match("^(lang-.*?):");
-    			curClass = curClass + " prettyprint";
-    			if(found){
-    				curClass = curClass+" "+found[1];
-    				codeBlock.innerHTML = codeBlock.innerHTML.replace(/^lang-.*?:/i,"");
-    			}
+    function updatePretty() {
+        var codeBlocks = $("pre, code");
+        var rePretty = false;
+        for (var i = 0; i < codeBlocks.length; i++) {
+            var codeBlock = codeBlocks[i];
+            var curClass = codeBlock.getAttribute('class') || "";
+            if (!curClass.includes("prettyprint")) {
+                rePretty = true;
+                var found = $(codeBlock).text().toLowerCase().match("^(lang-.*?):");
+                curClass = curClass + " prettyprint";
+                if (found) {
+                    curClass = curClass + " " + found[1];
+                    codeBlock.innerHTML = codeBlock.innerHTML.replace(/^lang-.*?:/i, "");
+                }
 
-    			codeBlock.setAttribute("class", curClass);
-    		}
-    	}
+                codeBlock.setAttribute("class", curClass);
+            }
+        }
 
-    	var expanders = $(".more-data");
+        var expanders = $(".more-data");
 
-    	for (var exp = 0; exp < expanders.length; exp++){
-    		var expander = expanders[exp];
-    		if (!expander.isBound){
-    			expander.isBound = true;
-    			$(expander).click(function(){
-    				var tied = $(this.parentNode).find("pre")[0];
-    				setTimeout(function(){
-    					var olClass = tied.getAttribute("class");
-    					tied.setAttribute("class",olClass.replace("prettyprinted",""));
-    					PR.prettyPrint();
-    				},1000);
-    			});
-    		}
-    	}
+        for (var i = 0; i < expanders.length; i++) {
+            var expander = expanders[i];
+            if (!expander.isBound) {
+                expander.isBound = true;
+                $(expander).click(function() {
+                    var tied = $(this.parentNode).find("pre")[0];
+                    setTimeout(function() {
+                        var olClass = tied.getAttribute("class");
+                        tied.setAttribute("class", olClass.replace("prettyprinted", ""));
+                        PR.prettyPrint();
+                    }, 1000);
+                });
+            }
+        }
 
-    	if(rePretty){
-    		PR.prettyPrint();
-    	}
+        if (rePretty)
+            PR.prettyPrint();
     }
 
-    setInterval(updatePretty,1000);
+    setInterval(updatePretty, 1000);
 })();
--- chatcommands.user.js
+++ chatcommands.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chat Commands
 // @namespace    http://tampermonkey.net/
-// @version      0.2.4
+// @version      0.2.5
 // @description  Add some simple ascii art commands.
 // @author       Teh Flamin' Taco
 // @match        *://*chat.stackexchange.com/*
@@ -12,24 +12,28 @@
 (function() {
     'use strict';
 
-    // Your code here...
-    
-    var codes={shrug: "¯\\\\_(ツ)_/¯",
-               tableflip: "(ノ°Д°）ノ︵ ┻━┻",
-               o_o: "ಠ_ಠ",
-               disapprove: "ಠ_ಠ",
-               like: "(•◡•)/",
-               unflip: "┬─┬﻿ ノ( ゜-゜ノ)",
-               donger: "༼ つ ◕_◕ ༽つ",
-               why: "ლ(ಠ益ಠლ)",
-               cool: "(▀̿Ĺ̯▀̿ ̿)",
-               lenny: "( ͡° ͜ʖ ͡°)",
-               ["\\$$(.+?)\\$"]: function(_,s){return "http://a-ta.co/mathjax/$$"+encodeURI(s)+".svg";},
-               ["\\$(.+?)\\$"]: function(_,s){return "http://a-ta.co/mathjax/$"+encodeURI(s)+".svg";}};
-    setInterval(function(){
-        for (var code in codes){
-            var x = document.getElementById("input");
-            x.value = x.value.replace(new RegExp("/"+code),codes[code]);
+    var codes = {
+        shrug: "¯\\\\_(ツ)_/¯",
+        tableflip: "(ノ°Д°）ノ︵ ┻━┻",
+        o_o: "ಠ_ಠ",
+        disapprove: "ಠ_ಠ",
+        like: "(•◡•)/",
+        unflip: "┬─┬ ノ( ゜-゜ノ)",
+        donger: "༼ つ ◕_◕ ༽つ",
+        why: "ლ(ಠ益ಠლ)",
+        cool: "(▀̿Ĺ̯▀̿ ̿)",
+        lenny: "( ͡° ͜ʖ ͡°)",
+        ["\\$$(.+?)\\$"]: function(_, s) {
+            return "http://a-ta.co/mathjax/$$" + encodeURI(s) + ".svg";
+        },
+        ["\\$(.+?)\\$"]: function(_, s) {
+            return "http://a-ta.co/mathjax/$" + encodeURI(s) + ".svg";
         }
-    },300);
+    };
+    setInterval(function() {
+        for (var code in codes) {
+            var x = document.getElementById("input");
+            x.value = x.value.replace(new RegExp("/" + code), codes[code]);
+        }
+    }, 300);
 })();
