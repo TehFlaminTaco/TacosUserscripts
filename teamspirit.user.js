7b603f1b4c695ecbbfcf72484b975a8f8641b3ac
William Lemon <William.Lemon2@gmail.com>
2017-05-02 09:44:43 +1000

    Re Introduce Mego's fix
    Why that vanished, I'll never know.

--- teamspirit.user.js
+++ teamspirit.user.js
@@ -13,68 +13,75 @@
 
 /* global $ */
 (function() {
-	'use strict';
+'use strict';
 
-	function getHref(jEle) {
-		return jEle.attr("href") || getHref(jEle.parent());
-	}
+function getHref(jEle) {
+	return jEle.attr("href") || getHref(jEle.parent());
+}
 
-	setInterval(function() {
+setInterval(function() {
 
-		$(".username").each((a, b) => {
-			var jB = $(b);
-			if (!jB.find(".stars").length) {
-				jB.prepend(`<span class='stars vote-count-container'><span class='img vote' style='background-image: url(${(getHref(jB).match(/\/users\/(\d+)/)||["","0"])[1]%2===0 ? 'http://i.imgur.com/6RZ23Ak.png' : 'http://i.imgur.com/6jKoAti.png'}) !important; background-position:0px 0px; background-size:10px 10px !important;'></span></span>`);
+	 $(".username").each((a, b) => {
+        var jB = $(b);
+        if($(b).parent(".user-popup").length) {
+            var href = $(b).parent(".user-popup").children("a");
+            if (!$(b).find(".stars").length) {
+                jB.prepend(`<span class='stars vote-count-container'><span class='img vote' style='background-image: url(${($(href).attr("href").match(/\/users\/(\d+)/)||["","0"])[1]%2===0 ? 'http://i.imgur.com/6RZ23Ak.png' : 'http://i.imgur.com/6jKoAti.png'}) !important; background-position:0px 0px; background-size:10px 10px !important;'></span></span>`);
+            }
+        } else {
+            if (!jB.find(".stars").length) {
+                jB.prepend(`<span class='stars vote-count-container'><span class='img vote' style='background-image: url(${(getHref(jB).match(/\/users\/(\d+)/)||["","0"])[1]%2===0 ? 'http://i.imgur.com/6RZ23Ak.png' : 'http://i.imgur.com/6jKoAti.png'}) !important; background-position:0px 0px; background-size:10px 10px !important;'></span></span>`);
+            }
+        }
+    });
+
+	$(".votesummary .collapsible").children()
+	.each(
+		(a,b)=>{
+			var links = $(b).find('a');
+			var jLink = $(links[links.length-1]);
+			if(!jLink.find(".stars").length){
+				jLink.prepend(`<span class='stars vote-count-container'><span class='img vote' style='background-image: url(${links[links.length-1].getAttribute("href").match(/users\/(\d+)/)[1]%2===0 ? 'http://i.imgur.com/6RZ23Ak.png' : 'http://i.imgur.com/6jKoAti.png'}) !important; background-position:0px 0px; background-size:10px 10px !important;'></span></span>`);
 			}
-		});
+        });
 
-		$(".votesummary .collapsible").children()
+	var x = $(".user-container");
+	var cRed = 0;
+	var cBlue = 0;
+
+	$(".votesummary .collapsible").children()
 		.each(
-			(a,b)=>{
+			(a, b) => {
+				var votes = $(b).find(".vote-count-container .times").text();
 				var links = $(b).find('a');
-				var jLink = $(links[links.length-1]);
-				if(!jLink.find(".stars").length){
-					jLink.prepend(`<span class='stars vote-count-container'><span class='img vote' style='background-image: url(${links[links.length-1].getAttribute("href").match(/users\/(\d+)/)[1]%2===0 ? 'http://i.imgur.com/6RZ23Ak.png' : 'http://i.imgur.com/6jKoAti.png'}) !important; background-position:0px 0px; background-size:10px 10px !important;'></span></span>`);
-				}
-            });
+				if (votes === "")
+					votes = 1;
+				if (links[links.length - 1].getAttribute("href").match(/users\/(\d+)/)[1] % 2 === 0)
+					cRed += Number(votes);
+				else
+					cBlue += Number(votes);
+			}
+		);
 
-		var x = $(".user-container");
-		var cRed = 0;
-		var cBlue = 0;
+	var total = cRed + cBlue;
 
-		$(".votesummary .collapsible").children()
-			.each(
-				(a, b) => {
-					var votes = $(b).find(".vote-count-container .times").text();
-					var links = $(b).find('a');
-					if (votes === "")
-						votes = 1;
-					if (links[links.length - 1].getAttribute("href").match(/users\/(\d+)/)[1] % 2 === 0)
-						cRed += Number(votes);
-					else
-						cBlue += Number(votes);
-				}
-			);
+	var redPercent = Math.ceil((cRed / total) * 100);
+	var bluPercent = Math.floor((cBlue / total) * 100);
+    $("#scoreholder").attr("title", "Red: " + cRed + ", Blue: " + cBlue);
+    $("#red_score_text").text(cRed);
+    $("#blue_score_text").text(cBlue);
+	$("#redscore").css({
+		width: `${redPercent}%`
+	});
+	$("#bluescore").css({
+		width: `${bluPercent}%`
+	});
+}, 2000);
 
-		var total = cRed + cBlue;
+$(".chat-input").append('<div id="scoreholder"><div id="redscore" class="scoreholder"></div><div id="bluescore" class="scoreholder"></div></div>');
+$("#chat-buttons").append(`<div id="scores"><span id="red_score_text">?</span>/<span id="blue_score_text">?</span></div>`);
 
-		var redPercent = Math.ceil((cRed / total) * 100);
-		var bluPercent = Math.floor((cBlue / total) * 100);
-        $("#scoreholder").attr("title", "Red: " + cRed + ", Blue: " + cBlue);
-        $("#red_score_text").text(cRed);
-        $("#blue_score_text").text(cBlue);
-		$("#redscore").css({
-			width: `${redPercent}%`
-		});
-		$("#bluescore").css({
-			width: `${bluPercent}%`
-		});
-	}, 2000);
-
-	$(".chat-input").append('<div id="scoreholder"><div id="redscore" class="scoreholder"></div><div id="bluescore" class="scoreholder"></div></div>');
-	$("#chat-buttons").append(`<div id="scores"><span id="red_score_text">?</span>/<span id="blue_score_text">?</span></div>`);
-
-	$("head").append(`<style>
+$("head").append(`<style>
 #scoreholder{
 	height:3px;
 	padding:1px;
