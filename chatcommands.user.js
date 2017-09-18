e86d1782993742890a4bc7e6ead5127a67f547fc
pavelbraginskiy <pavelbraginskiy@hotmail.com>
2017-09-17 21:03:00 -0700

    Fix /o_o bug

--- chatcommands.user.js
+++ chatcommands.user.js
@@ -54,7 +54,7 @@
     setInterval(function() {
         for (var code in codes) {
             var x = document.getElementById("input");
-            x.value = x.value.replace(new RegExp("(?:\\s|^)/" + code), codes[code]);
+            x.value = x.value.replace(new RegExp("(?<=\\s|^)/" + code), codes[code]);
         }
     }, 300);
 })();
